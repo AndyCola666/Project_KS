67b90496a43335557a95427ea0a99e2500d5b9d4
unknown <ganontars@hotmail.com>
2025-09-04 14:39:08 -0600

    videosDB10

--- yt.rb
+++ yt.rb
@@ -1,5 +1,5 @@
 def descargar_video(url)
-  carpeta = "D:/pruebakaraoke/Artistas/Paramore"
+  carpeta = "D:/pruebakaraoke/Artistas/Mecano"
 
   unless Dir.exist?(carpeta)
     puts "La carpeta '#{carpeta}' no existe."
